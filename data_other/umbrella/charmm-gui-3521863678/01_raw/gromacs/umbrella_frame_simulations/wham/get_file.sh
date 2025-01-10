1e2b76324db7825b1266d1e0035f082a89db50a5
Giacomo Marcon <marcongiacomo@gmail.com>
2025-01-10 18:37:59 +0100

    completed umbrella

--- data_other/umbrella/charmm-gui-3521863678/01_raw/gromacs/umbrella_frame_simulations/wham/get_file.sh
+++ data_other/umbrella/charmm-gui-3521863678/01_raw/gromacs/umbrella_frame_simulations/wham/get_file.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-arrstr="$(ls ../simulations_extra3/ | awk -F '_' 'BEGIN{a = ""}{a = a " " $2}END{print a}')"
+arrstr="$(ls ../simulations_extra4/ | awk -F '_' 'BEGIN{a = ""}{a = a " " $2}END{print a}')"
 
 IFS=" " read -r -a arr <<< "$arrstr"
 
 for str in ${arr[@]}; do
     echo $str
-    path="../simulations_extra3/results/sim_"$str"/mdrun_pullf.xvg"
+    path="../simulations_extra4/results/sim_"$str"/mdrun_pullf.xvg"
     ptarg="pullf/umbrella"$str"_pullf.xvg"
     cp $path $ptarg
-    path="../simulations_extra3/results/sim_"$str"/mdrun.tpr"
+    path="../simulations_extra4/results/sim_"$str"/mdrun.tpr"
     ptarg="tpr/umbrella"$str".tpr"
     cp $path $ptarg
 done
